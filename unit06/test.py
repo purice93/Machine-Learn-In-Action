1d5f89a6f2f09f4f78a095fc109fbc7e8fd70672
purice93 <530337704@qq.com>
2018-12-06 20:17:33 +0800

    svm在理解-公式

--- unit06/test.py
+++ unit06/test.py
@@ -2,7 +2,14 @@
 @author: zoutai
 @file: test.py 
 @time: 2017/11/13 
-@description: 
+@description:
+
+博客参考：
+https://blog.csdn.net/c406495762/article/details/78072313
+https://blog.csdn.net/c406495762/article/details/78158354
+
+SVM之松弛变量和惩罚因子：
+https://www.jianshu.com/p/8a499171baa9
 """
 
 import matplotlib.pyplot as plt
@@ -25,45 +32,43 @@ def showDataSet(dataMat, labelMat):
     plt.show()
 
 
-def showClassifer(dataMat, w, b):
-    '''
+def showClassifer(alphas,dataMat, classLabels, w, b):
+    """
     分类结果可视化
-    Args:
-        dataMat:
-        w:直线法向量
-        b:直线解决
-
+    Parameters:
+        dataMat - 数据矩阵
+        w - 直线法向量
+        b - 直线解决
     Returns:
-
-    '''
-    # 绘制样本点
-    data_plus = []  # 正样本
-    data_minus = []  # 负样本
+        无
+    """
+    #绘制样本点
+    data_plus = []                                  #正样本
+    data_minus = []                                 #负样本
     for i in range(len(dataMat)):
-        if labelMat[i] > 0:
+        if classLabels[i] > 0:
             data_plus.append(dataMat[i])
         else:
             data_minus.append(dataMat[i])
-    data_plus_np = np.array(data_plus)  # 转换为numpy矩阵
-    data_minus_np = np.array(data_minus)  # 转换为numpy矩阵
-    plt.scatter(np.transpose(data_plus_np)[0], np.transpose(data_plus_np)[1], s=30, alpha=0.7)  # 正样本散点图
-    plt.scatter(np.transpose(data_minus_np)[0], np.transpose(data_minus_np)[1], s=30, alpha=0.7)  # 负样本散点图
-    # 绘制直线
+    data_plus_np = np.array(data_plus)              #转换为numpy矩阵
+    data_minus_np = np.array(data_minus)            #转换为numpy矩阵
+    plt.scatter(np.transpose(data_plus_np)[0], np.transpose(data_plus_np)[1], s=30, alpha=0.7)   #正样本散点图
+    plt.scatter(np.transpose(data_minus_np)[0], np.transpose(data_minus_np)[1], s=30, alpha=0.7) #负样本散点图
+    #绘制直线
     x1 = max(dataMat)[0]
     x2 = min(dataMat)[0]
     a1, a2 = w
     b = float(b)
     a1 = float(a1[0])
     a2 = float(a2[0])
-    y1, y2 = (-b - a1 * x1) / a2, (-b - a1 * x2) / a2
+    y1, y2 = (-b- a1*x1)/a2, (-b - a1*x2)/a2
     plt.plot([x1, x2], [y1, y2])
-    # 找出支持向量点
+    #找出支持向量点
     for i, alpha in enumerate(alphas):
         if abs(alpha) > 0:
             x, y = dataMat[i]
             plt.scatter([x], [y], s=150, c='none', alpha=0.7, linewidth=1.5, edgecolor='red')
     plt.show()
-
 
 def get_w(dataMat, labelMat, alphas):
     alphas, dataMat, labelMat = np.array(alphas), np.array(dataMat), np.array(labelMat)
@@ -72,16 +77,16 @@ def get_w(dataMat, labelMat, alphas):
 
 
 # 实验1：可视化初始数据
-# dataMat, labelMat = loadDataSet('testSet.txt')
+dataMat, labelMat = loadDataSet('testSet.txt')
 
 
 # showDataSet(dataMat, labelMat)
 
 
 # 实验2：可视化支持向量点-简化版SMO
-# b, alphas = smoSimple(dataMat, labelMat, 0.6, 0.001, 40)
-# w = get_w(dataMat, labelMat, alphas)
-# showClassifer(dataMat, w, b)
+b, alphas = smoSimple(dataMat, labelMat, 0.6, 0.001, 40)
+w = get_w(dataMat, labelMat, alphas)
+showClassifer(alphas,dataMat, labelMat, w, b)
 
 
 # 太难，暂时先熟悉完理论，代码一脸懵逼。。
@@ -106,9 +111,9 @@ def calcWs(alphas, dataArr, classLabels):
 
 
 # test2-p103
-# b, alphas = smoP(dataMat, labelMat, 0.6, 0.001, 40)
-# w = calcWs(alphas, dataMat, labelMat)
-# showClassifer(dataMat, w, b)
+b, alphas = smoP(dataMat, labelMat, 0.6, 0.001, 40)
+w = calcWs(alphas, dataMat, labelMat)
+showClassifer(alphas,dataMat, labelMat, w, b)
 
 # test3-p109
 # dataMat, labelMat = loadDataSet('testSetRBF.txt')                      #加载训练集
@@ -116,4 +121,4 @@ def calcWs(alphas, dataArr, classLabels):
 # testRbf()
 
 # test4-p113
-testDigits(('rbf',20))
+# testDigits(('rbf',20))
